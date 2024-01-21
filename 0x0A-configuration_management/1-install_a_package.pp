4224d92819c2cc567e2c2d50df34cb9ec1fe41ea
Armolas <armolas06@gmail.com>
2024-01-21 16:29:30 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # installs flask version 2.1.0
-package { 'python3-pip':
-  ensure     => installed,
-}
 
 package{ 'flask':
-    name     => 'flask'
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip']
 }
