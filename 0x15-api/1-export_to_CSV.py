513cdad28b149f9f45cef3ce445307cb17c47be4
Armolas <armolas06@gmail.com>
2024-02-19 14:30:09 +0100

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""exports user data in csv format"""
+import requests
+import sys
+
+
+def create_csv(user_id):
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    users = requests.get(user_url)
+    users = users.json()
+    todos = requests.get(todo_url)
+    todos = todos.json()
+    filename = f"{user_id}.csv"
+    for user in users:
+        if user['id'] == user_id:
+            name = user['name']
+            break
+    with open(filename, 'w') as file:
+        for todo in todos:
+            if todo['userId'] == user_id:
+                line = f"\"{str(user_id)}\",\"{str(name)}\",\
+\"{todo['completed']}\",\"{todo['title']}\"\n"
+                file.write(line)
+
+
+if __name__ == '__main__':
+    create_csv(int(sys.argv[1]))
