5c1c42d3440aa1c687d6203c18691a0a9d962952
Armolas <armolas06@gmail.com>
2024-02-19 13:33:48 +0100

    task_0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""returns information about an employee todo list"""
+import requests
+import sys
+
+
+def get_todo(userId):
+    """gets information about a user todo using userID"""
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+    response = requests.get(url)
+    users = requests.get(users_url)
+    todos = response.json()
+    users = users.json()
+    completed = 0
+    task_no = 0
+    name = ''
+    tasks = []
+    for todo in todos:
+        if todo['userId'] == userId:
+            if todo['completed'] == True:
+                completed += 1
+                tasks.append(todo['title'])
+            task_no += 1
+    for user in users:
+        if user['id'] == userId:
+            name = user['name']
+            break
+    print(f"Employee {name} is done with tasks({completed}/{task_no})")
+    for task in tasks:
+        print(f"\t {task}")
+get_todo(int(sys.argv[1]))
