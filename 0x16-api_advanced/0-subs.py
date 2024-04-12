76a004be4aa0c1393ae66c3de1013d0728e98868
Armolas <armolas06@gmail.com>
2024-04-12 12:12:48 +0100

    task_0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers (not active users,
+    total subscribers) for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {'User-Agent': 'Armolas_pc'}
+    response = requests.get(url, allow_redirects=False, headers=header)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
