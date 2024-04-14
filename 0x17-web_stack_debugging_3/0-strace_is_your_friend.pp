0af1ee7a33fa604bcd646c34c12b13db159fb570
Armolas <armolas06@gmail.com>
2024-04-15 00:47:03 +0100

    fixed_puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,10 @@
 # Define a resource to manage the specific file
-file { '/path/to/your/file':
+file { '/var/www/html/wp-settings.php':
+
+  ensure  => present,
 
   # Replace a specific line based on a pattern
   replace => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );',
   # Content to replace the matched line with
-  with    => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );',
-
-  # Ensure the file is present before making changes
-  ensure  => present,
+  with    => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );'
 }
