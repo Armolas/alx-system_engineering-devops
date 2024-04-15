17b8f0d333f2ed8b29c526e454e8bb88c946592f
Armolas <armolas06@gmail.com>
2024-04-15 01:50:10 +0100

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,5 @@
 # Define a resource to manage the specific file
-file { '/var/www/html/wp-settings.php':
-
-  ensure  => present,
-
-  # Replace a specific line based on a pattern
-  replace => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );',
-  # Content to replace the matched line with
-  with    => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );'
+exec{ 'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
