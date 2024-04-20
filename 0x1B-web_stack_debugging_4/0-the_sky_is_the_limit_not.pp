e11e3e337374f614e3e7343fbcc5c345b57d84ee
Armolas <armolas06@gmail.com>
2024-04-20 13:00:22 +0100

    fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #changes the ulimit of nginx
 exec{ 'replace':
     provider => shell,
-    command  => 'sed -i s/15/5000/g /etc/default/nginx'
+    command  => 'sed -i s/15/5000/g /etc/default/nginx && service nginx restart'
 }
